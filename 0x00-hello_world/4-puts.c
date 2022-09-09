4b94d6d05844878b4329f77fb257ab2189006f34
calvary303 <nellyalbert303@gmail.com>
2022-09-08 19:32:02 -0700

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,16 @@
  *
  *  * main - main block
  *
- *   * Return: 0
+ *  * Return: 0
  *
- *    */
+ *  */
 
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
