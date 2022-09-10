69d69b2393126ab07b1b7afd78935e712c8e8b10
calvary303 <nellyalbert303@gmail.com>
2022-09-09 21:44:28 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,18 +5,18 @@
 * Description: using the main function
 * this program prints "Programming is positive, zero, or negative
 * Return: 0
- */
+*/
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
+char c, h;
+for (c = 'a' ; c <= 'z' ; c++)
 {
-	putchar(ch);
+	putchar(c);
 }
-for (ch = 'A' ; ch <= 'Z' ; ch++)
+for (h = 'A' ; h <= 'Z' ; h++)
 {
-	putchar(ch);
+	putchar(h);
 }
-putchar('\n')
+putchar(10)
 return (0);
 }
